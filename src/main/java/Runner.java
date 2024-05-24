4cd36c6f1048e6695a68f6af30222b90f11c0367
Gözde Kızılkaya <kizilkayagozde@gmail.com>
2024-05-24 22:43:28 +0300

    v2

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -5,5 +5,9 @@ public class Runner {
         System.out.println("hello GIT");
         System.out.println("git add . : staging area'ya alır.");
 
+        //versiyon 2
+        System.out.println("git commit -m\"\" ile versiyon oluşturulur.");
+
+
     }
 }
